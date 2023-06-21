deb4337f0846963a9521d5f63c95c467f61f5cea
sasholilov <alex_xld@abv.bg>
2023-06-22 00:25:09 +0300

    update css and add getcarsforcurrnet user to select in docs

--- src/components/documents/modalAddDocs.jsx
+++ src/components/documents/modalAddDocs.jsx
@@ -1,6 +1,21 @@
 import "./modalAddDocs.css";
+import { useState, useEffect } from "react";
+import { getCarsForCurrentUser } from "../../firebase-config";
 
 export const ModalAddDocs = ({ setOpenModal }) => {
+  const [myCars, setMycars] = useState([]);
+  const [choisedDoc, setChoisedDoc] = useState("Избери вид документ");
+
+  useEffect(() => {
+    getCarsForCurrentUser()
+      .then((cars) => {
+        setMycars(cars);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -14,14 +29,22 @@ export const ModalAddDocs = ({ setOpenModal }) => {
           </button>
         </div>
         <div className="modal-body">
-          <label id="label-docs-first">Избере тип документ</label>
+          <h2>{choisedDoc}</h2>
+          <label>За автомобил</label>
           <select>
+            {myCars.map((car, i) => (
+              <option key={i}>
+                {car.make} {car.model} {car.year}
+              </option>
+            ))}
+          </select>
+          <label>Избере тип документ</label>
+          <select onChange={(e) => setChoisedDoc(e.target.value)}>
             <option>Винетка</option>
             <option>Гражданска отговорност</option>
             <option>ГТП</option>
             <option>Автокаско</option>
           </select>
-
           <label>Валиден от</label>
           <input type="date"></input>
           <label>Валиден до</label>
@@ -35,9 +58,9 @@ export const ModalAddDocs = ({ setOpenModal }) => {
             }}
             id="cancelBtn"
           >
-            Cancel
+            Откажи
           </button>
-          <button>Add</button>
+          <button>Добави</button>
         </div>
       </div>
     </div>
