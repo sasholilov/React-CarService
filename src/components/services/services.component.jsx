0c2b4e32fa0401218c3df47e12cc1d8b76f6a56c
sasholilov <alex_xld@abv.bg>
2023-06-30 01:02:43 +0300

    import google maps in services

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -2,13 +2,24 @@ import { useEffect, useState, useContext } from "react";
 import { getDataFromFirestore, db } from "../../firebase-config";
 import { updateDoc, collection, getDoc, doc } from "firebase/firestore";
 import UserContext from "../context/userContext";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { Loading } from "../loading/loading.component";
 import "./services.style.css";
 
 export const Services = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+  if (!isLoaded) return <Loading />;
   return (
     <div className="header-services">
-      <h1>Добави сервиз</h1>
-      <button>Добави</button>
+      <GoogleMap
+        zoom={10}
+        center={{ lat: 42.7094406, lng: 23.2823135 }}
+        mapContainerClassName="map-container"
+      >
+        <Marker position={{ lat: 42.7094406, lng: 23.2823135 }}></Marker>
+      </GoogleMap>
     </div>
   );
 };
