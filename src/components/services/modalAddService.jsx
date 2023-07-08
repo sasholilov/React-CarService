f20755cbc10b3e6750c967aca63d6011ab9f70ea
sasholilov <alex_xld@abv.bg>
2023-07-08 15:10:44 +0300

    save services and place on map

--- src/components/services/modalAddService.jsx
+++ src/components/services/modalAddService.jsx
@@ -1,3 +1,149 @@
-export const modalAddService = () => {
-  return <div></div>;
+import { useEffect, useState, useContext, useRef } from "react";
+import { geocodeAddress } from "./getGeoCodeAddress";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { db, getDataFromFirestore } from "../../firebase-config";
+import { collection, updateDoc, arrayUnion } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import UserContext from "../context/userContext";
+import { Loading } from "../loading/loading.component";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "./modalServices.style.css";
+
+export const ModalAddService = ({ setOpenModal }) => {
+  const [coordinates, setCoordinates] = useState({
+    latitude: 42.697863,
+    longitude: 23.322179,
+  });
+  const currentUser = useContext(UserContext);
+  const [nameOfService, setNameOfService] = useState("");
+  const [city, setCity] = useState("София");
+  const [addressServices, setAddressService] = useState("");
+  const [telephone, setTelephone] = useState("");
+  const [center, setCenter] = useState([42.697863, 23.322179]);
+  const [isNewAddress, setIsNewAdrress] = useState(false);
+  const customIcon = new Icon({
+    iconUrl: require("./../../img/marker-icon.png"),
+    iconSize: [38, 38],
+  });
+  const mapRef = useRef();
+  const address = `${addressServices},${city}`;
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      const result = await geocodeAddress(address);
+      console.log("result", result);
+      setCoordinates(result);
+      setIsNewAdrress(true);
+    };
+
+    fetchCoordinates();
+  }, [address]);
+
+  useEffect(() => {
+    if (isNewAddress) {
+      setCenter((prevCenter) => [coordinates.latitude, coordinates.longitude]);
+      const map = mapRef.current;
+      if (map) {
+        map.panTo(center, { animate: true });
+      }
+    }
+  }, [address]);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  const addServiceToFirestore = async () => {
+    const service = {
+      nameOfService: nameOfService,
+      city: city,
+      address: addressServices,
+      telephone: telephone,
+      coordinates: coordinates,
+    };
+    try {
+      const userCollection = collection(db, "users");
+      const userDocRef = doc(userCollection, currentUser.user.uid);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        // User document exists, update the 'cars' array field
+        await updateDoc(userDocRef, {
+          services: arrayUnion(service),
+        });
+      } else {
+        // User document doesn't exist, create a new document
+        await setDoc(userDocRef, {
+          services: [service],
+        });
+      }
+
+      setOpenModal(false);
+      console.log("Success added");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  console.log("center", center);
+
+  if (!isLoaded) return <Loading />;
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className="modal-body">
+          <h2>Регистрация на сервиз</h2>
+          <label>Име на сервиза</label>
+          <input onChange={(e) => setNameOfService(e.target.value)} />
+          <label>Град</label>
+          <input onChange={(e) => setCity(e.target.value)} />
+          <label>Адрес</label>
+          <input onChange={(e) => setAddressService(e.target.value)} />
+          <label>Телефонен номер</label>
+          <input onChange={(e) => setTelephone(e.target.value)} />
+        </div>
+        <div className="footer">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            id="cancelBtn"
+          >
+            Откажи
+          </button>
+          <button onClick={addServiceToFirestore}>Добави</button>
+        </div>
+      </div>
+      <MapContainer
+        center={center}
+        zoom={13}
+        ref={mapRef}
+        style={{ width: "30%", height: "55%", margin: "20px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {coordinates.latitude && coordinates.longitude && (
+          <Marker
+            position={[coordinates.latitude, coordinates.longitude]}
+            icon={customIcon}
+          />
+        )}
+      </MapContainer>
+    </div>
+  );
 };
--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -2,93 +2,81 @@ import { useEffect, useState, useContext, useRef } from "react";
 import { geocodeAddress } from "./getGeoCodeAddress";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { Loading } from "../loading/loading.component";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { ModalAddService } from "./modalAddService";
 import { Icon } from "leaflet";
+import { getDataFromFirestore, db } from "../../firebase-config";
 import "leaflet/dist/leaflet.css";
 import "./services.style.css";
 
 export const Services = () => {
-  const [coordinates, setCoordinates] = useState({
-    latitude: 42.697863,
-    longitude: 23.322179,
-  });
-  const [city, setCity] = useState("София");
-  const [addressServices, setAddressService] = useState("");
-  const [telephone, setTelephone] = useState("");
+  const [openModal, setOpenModal] = useState(false);
+  const [myServices, setMyServices] = useState([]);
   const [center, setCenter] = useState([42.697863, 23.322179]);
-  const [isNewAddress, setIsNewAdrress] = useState(false);
   const customIcon = new Icon({
     iconUrl: require("./../../img/marker-icon.png"),
     iconSize: [38, 38],
   });
   const mapRef = useRef();
-  const address = `${addressServices},${city}`;
 
   useEffect(() => {
-    const fetchCoordinates = async () => {
-      const result = await geocodeAddress(address);
-      console.log("result", result);
-      setCoordinates(result);
-      setIsNewAdrress(true);
-    };
-
-    fetchCoordinates();
-  }, [address]);
+    getDataFromFirestore()
+      .then((data) => {
+        setMyServices(data.services);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, [openModal]);
+  console.log(myServices);
 
   useEffect(() => {
-    if (isNewAddress) {
-      setCenter((prevCenter) => [coordinates.latitude, coordinates.longitude]);
-      const map = mapRef.current;
-      if (map) {
-        map.panTo(center, { animate: true });
-      }
+    setCenter((prevCenter) => [center[0], center[1]]);
+    const map = mapRef.current;
+    if (map) {
+      map.panTo(center, { animate: false });
     }
-  }, [address]);
+  }, [center]);
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  });
-
-  console.log("center", center);
-
-  if (!isLoaded) return <Loading />;
   return (
-    <div className="header-services">
-      <input
-        placeholder="Въведи град"
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <input
-        placeholder="Въведи адрес"
-        onChange={(e) => setAddressService(e.target.value)}
-      />
-      <input
-        placeholder="Телефонен номер"
-        onChange={(e) => setTelephone(e.target.value)}
-      />
-      <MapContainer center={center} zoom={13} ref={mapRef}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {coordinates.latitude && coordinates.longitude && (
-          <Marker
-            position={[coordinates.latitude, coordinates.longitude]}
-            icon={customIcon}
+    <div>
+      <div className="header-services">
+        <h1>Добави нов сервиз</h1>
+        <button onClick={() => setOpenModal(true)}>Добави</button>
+        {openModal && <ModalAddService setOpenModal={setOpenModal} />}
+      </div>
+      {myServices?.map((s, i) => (
+        <h2
+          onClick={() =>
+            setCenter([s.coordinates.latitude, s.coordinates.longitude])
+          }
+          key={i}
+        >
+          {s.nameOfService}
+        </h2>
+      ))}
+      {openModal ? null : (
+        <MapContainer center={center} zoom={13} ref={mapRef}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        )}
-      </MapContainer>
 
-      {/*<GoogleMap
-        zoom={10}
-        center={{ lat: coordinates.latitude, lng: coordinates.longitude }}
-        mapContainerClassName="map-container"
-      >
-        <Marker
-          position={{ lat: coordinates.latitude, lng: coordinates.longitude }}
-        ></Marker>
-      </GoogleMap>*/}
+          {myServices.length > 0 &&
+            myServices.map((m) => (
+              <Marker
+                position={[m.coordinates.latitude, m.coordinates.longitude]}
+                icon={customIcon}
+              >
+                <Popup>
+                  {`${m.nameOfService}, 
+                  ${m.city}, ${m.address}, 
+                  ${m.telephone}`}
+                </Popup>
+              </Marker>
+            ))}
+        </MapContainer>
+      )}
     </div>
   );
 };
