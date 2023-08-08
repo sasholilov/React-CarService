7ac243392f9ccc19478380c6b5246e814103d1fa
sasholilov <alex_xld@abv.bg>
2023-08-08 22:37:13 +0300

    added new buttons

--- src/components/buttons/buttons.component.jsx
+++ src/components/buttons/buttons.component.jsx
@@ -1,12 +1,37 @@
 import "./buttons.style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCirclePlus,
+  faBan,
+  faPen,
+  faTrash,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Buttons = ({ buttonStyle, onPush }) => {
   if (buttonStyle === "add")
     return (
       <em className="button-add" onClick={onPush}>
         <FontAwesomeIcon icon={faCirclePlus} />
+      </em>
+    );
+  if (buttonStyle === "cancel")
+    return (
+      <em className="button-cancel" onClick={onPush}>
+        <FontAwesomeIcon icon={faXmark} />
+      </em>
+    );
+  if (buttonStyle === "edit")
+    return (
+      <em className="button-edit" onClick={onPush}>
+        <FontAwesomeIcon icon={faPen} />
+      </em>
+    );
+
+  if (buttonStyle === "delete")
+    return (
+      <em className="button-delete" onClick={onPush}>
+        <FontAwesomeIcon icon={faTrash} />
       </em>
     );
 };
